606c189e2b64efe87b828bf8037c02c22aed3d31
Ohoud Alawad <ohoudalawad@gmail.com>
2022-12-22 14:43:37 +0300

    Solved polar coordinates Tsk7

---
+++ polar coordinates Task7.py
@@ -0,0 +1,10 @@
+#Task
+#You are given a complex . Your task is to convert it to polar coordinates.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import cmath
+
+r, phi = cmath.polar(complex(input()))
+
+print(r)
+print(phi)
