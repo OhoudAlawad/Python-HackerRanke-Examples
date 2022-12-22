8cbf0e88092a465d6173a76f35274615ed3eb25b
Ohoud Alawad <ohoudalawad@gmail.com>
2022-12-22 22:56:53 +0300

    Solved pow Task10

---
+++ pow.py
@@ -0,0 +1,11 @@
+#Task 10
+#You are given three integers: , , and . Print two lines.
+#On the first line, print the result of pow(a,b). On the second line, print the result of pow(a,b,m).
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(pow(a,b))
+
+print(pow(a,b,m))
