e553e7af13eb15784f0ae19209e8947dd482c812
Ohoud Alawad <ohoudalawad@gmail.com>
2022-12-22 23:08:24 +0300

    Solved calculations on very large integers Task11

---
+++ calculations on very large integers.py
@@ -0,0 +1,14 @@
+#Task 11
+# Read four numbers,a ,b ,c ,d and , and print the result of (a power b + c power d) .
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print((a**b)+(c**d))
+
+
